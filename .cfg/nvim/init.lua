dc11512145aa9de3e76589d447e68892be7f2362
givememymilk <118409646+givememymilk@users.noreply.github.com>
2026-07-31 10:16:15 +0000

    duke nukem

---
+++ .cfg/nvim/init.lua
@@ -0,0 +1,17 @@
+vim.opt.termguicolors = false
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    local groups = { "Normal", "NonText", "SignColumn", "LineNr", "EndOfBuffer" }
+    for _, group in ipairs(groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+    end
+  end,
+})
+
+vim.cmd("colo lunaperche") 
+
+local o = vim.opt
+
+o.number = true
